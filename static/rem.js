9f907a6ea8566b8a0cd07f9bcbf14d41b3586dec
zhongchenguang <1244977965@qq.com>
2019-10-29 13:49:01 +0800

    rem

--- static/rem.js
+++ static/rem.js
@@ -2,7 +2,7 @@
     var docEl = document.documentElement;
 
     function setRemUint(){
-        var rem = docEl.clientWidth*(window.devicePixelRatio||1) / 10;
+        var rem = docEl.clientWidth/ 10;
         docEl.style.fontSize = rem + 'px';
     }
 
